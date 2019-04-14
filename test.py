78b858a620a734154c4192779f4c352879f91745
Ren Yang <yangren@buaa.edu.cn>
2019-04-14 09:56:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,32 +3,40 @@ import tensorflow as tf
 import os
 import network
 import yuv_import
+import argparse
 
+parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--name")
+parser.add_argument("--frames", type=int)
+parser.add_argument("--height", type=int)
+parser.add_argument("--width", type=int)
 
-Height = 240
-Width = 416
+args = parser.parse_args()
+
+Height = args.height
+Width = args.width
+frames = args.frames
+name = args.name
+
 Channel = 1
 batch_size = 1
-frames = 500
-
 relu = 1
 kernel = [5, 5]
 filter_num = 24
 CNNlayer = 5
 
-sample_file = 1
+filename1 = './' + name + '_raw.yuv' #raw
+filename2 = './' + name + '_HEVC_QP42.yuv' #comp
 
-filename1 = './BasketballPass_Raw.yuv' #raw
-filename2 = './BasketballPass_HEVC_QP42.yuv' #comp
-
-step = 21
+step = 22
 
 def generate_weight(x_out):
 
     for s in range(step):
 
-        u_weight = tf.expand_dims(x_out, axis = -1)
-        u_weight = tf.expand_dims(u_weight, axis = -1)
+        u_weight = tf.expand_dims(x_out, axis=-1)
+        u_weight = tf.expand_dims(u_weight, axis=-1)
         u_weight = tf.expand_dims(u_weight, axis=-1)
 
         u_weight = tf.tile(u_weight, [1, 1, Height, Width, 1])
@@ -36,6 +44,7 @@ def generate_weight(x_out):
         f_weight = tf.ones([batch_size, step, Height, Width, Channel]) - u_weight
 
     return f_weight, u_weight
+
 
 def dense(x, step):
 
@@ -57,6 +66,7 @@ def dense(x, step):
 
     return x_o
 
+
 def sig(x):
 
     a = tf.get_variable('W/a', initializer=10.0)
@@ -65,16 +75,12 @@ def sig(x):
 
     return y, a
 
-# os.environ['CUDA_VISIBLE_DEVICES'] = os.environ["SGE_GPU"]
 
 config = tf.ConfigProto(allow_soft_placement = True)
 sess = tf.Session(config = config)
 
 x1 = tf.placeholder(tf.float32, [batch_size, step, Height, Width, Channel]) #raw
 x2 = tf.placeholder(tf.float32, [batch_size, step, Height, Width, Channel]) #compressed
-
-# forget = tf.placeholder(tf.float32, [batch_size, step, Height, Width, Channel]) #forget_weight
-# update = tf.placeholder(tf.float32, [batch_size, step, Height, Width, Channel]) #update_weight
 
 x_1 = tf.placeholder(tf.float32, [batch_size, step, 190]) #feat
 
@@ -91,7 +97,7 @@ forget, update = generate_weight(x_out)
 outputs = network.net_bi_wcell(x2, forget, update, step, Height, Width, filter_num, kernel, relu, CNNlayer, peephole = False, scale = False)
 
 saver = tf.train.Saver()
-saver.restore(sess, './QP42_model/model.ckpt-200000')
+saver.restore(sess, './QP42_bi_wcell_PQF_finetune/model.ckpt-200000')
 
 # num_params = 0
 # for variable in tf.trainable_variables():
@@ -100,7 +106,7 @@ saver.restore(sess, './QP42_model/model.ckpt-200000')
 #
 # print(num_params)
 
-feat = np.load('BasketballPass_HEVC_QP42_quality_features.npy')
+feat = np.load(name + '_HEVC_QP42_quality_features.npy')
 
 feat = np.concatenate((feat, feat[-1:]), axis=0)
 feat = np.concatenate((feat, feat[-1:]), axis=0)
@@ -113,7 +119,6 @@ for f in range(5):
     index = 0
 
     for s in range(frames):
-
 
         if frame_begin + s >= 2:
 
@@ -162,21 +167,32 @@ for f in range(5):
 
 d_psnr = np.zeros([frames])
 
-for seq in range(frames//(step-1)):
+for seq in range(frames//(step-2)):
 
-    if seq < frames // (step - 1) - 1:
-
-        [Y_rawv, U0, V0] = yuv_import.yuv_import(filename1, (Height, Width),
-                                                 step, seq*(step-1))
-        [Y_comp, U1, V1] = yuv_import.yuv_import(filename2, (Height, Width),
-                                                 step, seq*(step-1))
-
-    elif seq == frames//(step-1) - 1:
+    if seq == 0:
 
         [Y_rawv, U0, V0] = yuv_import.yuv_import(filename1, (Height, Width),
-                                                 step-1, seq * (step - 1))
+                                                 step-1, 0)
         [Y_comp, U1, V1] = yuv_import.yuv_import(filename2, (Height, Width),
-                                                 step-1, seq * (step - 1))
+                                                 step-1, 0)
+
+        Y_rawv = np.concatenate((Y_rawv[0:1], Y_rawv), axis=0)
+        Y_comp = np.concatenate((Y_comp[0:1], Y_comp), axis=0)
+        feature = np.concatenate((feature[:, 0:1, :], feature), axis=1)
+
+    elif seq < frames // (step - 2) - 1:
+
+        [Y_rawv, U0, V0] = yuv_import.yuv_import(filename1, (Height, Width),
+                                                 step, seq*(step-2)-1)
+        [Y_comp, U1, V1] = yuv_import.yuv_import(filename2, (Height, Width),
+                                                 step, seq*(step-2)-1)
+
+    elif seq == frames//(step-2) - 1:
+
+        [Y_rawv, U0, V0] = yuv_import.yuv_import(filename1, (Height, Width),
+                                                 step-1, seq * (step - 2)-1)
+        [Y_comp, U1, V1] = yuv_import.yuv_import(filename2, (Height, Width),
+                                                 step-1, seq * (step - 2)-1)
         Y_rawv = np.concatenate((Y_rawv, Y_rawv[-1:]), axis=0)
         Y_comp = np.concatenate((Y_comp, Y_comp[-1:]), axis=0)
         feature = np.concatenate((feature, feature[:, -1:, :]), axis=1)
@@ -187,9 +203,9 @@ for seq in range(frames//(step-1)):
     Y_rawv = Y_rawv[np.newaxis, :]
     Y_rawv = Y_rawv[:, :, :, :, np.newaxis] / 255.0
 
-    Y_enhanced = sess.run(outputs + x2, feed_dict={x2: Y_comp, x_1: feature[:, seq*(step-1):seq*(step-1) + step]})
+    Y_enhanced = sess.run(outputs + x2, feed_dict={x2: Y_comp, x_1: feature[:, seq*(step-2):seq*(step-2) + step]})
 
-    for f in range(step-1):
+    for f in range(1, step-1):
 
         mse = np.mean(np.power(np.subtract(Y_rawv[0, f:f + 1], Y_enhanced[0, f:f + 1]),2.0))
         mse0 = np.mean(np.power(np.subtract(Y_rawv[0, f:f + 1], Y_comp[0, f:f + 1]), 2.0))
@@ -197,10 +213,11 @@ for seq in range(frames//(step-1)):
         psnr = 10.0*np.log(1.0/mse)/np.log(10.0)
         psnr0 = 10.0 * np.log(1.0 / mse0) / np.log(10.0)
 
-        d_psnr[seq*(step-1) + f] = psnr - psnr0
+        d_psnr[seq*(step-2) + f - 1] = psnr - psnr0
 
-        print('Frame: ' + str(seq*(step-1) + f + 1) + '  Delta PSNR (dB): ' + str(psnr - psnr0))
+        print('Frame: ' + str(seq*(step-2) + f) + '  Delta PSNR (dB): ' + str(psnr - psnr0))
 
 print('Average Delta PSNR (dB):')
 print(np.mean(d_psnr))
+
 
